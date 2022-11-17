564deb51101f5dfff45a5ab8e5e5756e3e501720
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-17 18:19:57 +0200

    add switch view to main activity

--- app/src/main/java/com/rezkalla/backgroundlocationtrackingapp/MainActivity.kt
+++ app/src/main/java/com/rezkalla/backgroundlocationtrackingapp/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.rezkalla.backgroundlocationtrackingapp.location.LocationService
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  btnStart : Button
-    private lateinit var  btnStop : Button
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+    private lateinit var switchEnable: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +26,23 @@ class MainActivity : AppCompatActivity() {
             ),
             0
         )
-        btnStart= findViewById(R.id.buttonStart)
-        btnStop= findViewById(R.id.buttonStop)
+        btnStart = findViewById(R.id.buttonStart)
+        btnStop = findViewById(R.id.buttonStop)
+        switchEnable = findViewById(R.id.switch1)
         btnStart.setOnClickListener {
-            Intent(applicationContext,LocationService::class.java).apply {
+            Intent(applicationContext, LocationService::class.java).apply {
                 action = LocationService.ACTION_START
                 startService(this)
             }
         }
         btnStop.setOnClickListener {
-            Intent(applicationContext,LocationService::class.java).apply {
+            Intent(applicationContext, LocationService::class.java).apply {
                 action = LocationService.ACTION_STOP
                 startService(this)
             }
+        }
+        switchEnable.setOnCheckedChangeListener { _, isChecked ->
+            Toast.makeText(this, "isChecked: $isChecked", Toast.LENGTH_SHORT).show()
         }
     }
 }
