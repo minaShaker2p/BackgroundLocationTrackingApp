a8095f537739de28078a4393bac9aefd3e97a25d
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-17 18:06:11 +0200

    Create SharedPreferences with delegate

---
+++ app/src/main/java/com/rezkalla/backgroundlocationtrackingapp/utils/SharedPreferenecesUtils.kt
@@ -0,0 +1,33 @@
+package com.rezkalla.backgroundlocationtrackingapp.utils
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import kotlin.reflect.KProperty
+
+
+inline operator fun <reified T> SharedPreferences.getValue(thisRef: Any, property: KProperty<*>): T {
+
+    return when(T::class)
+    {
+        Boolean::class -> getBoolean(property.name, false)
+        Int::class -> getInt(property.name, 0)
+        Float::class -> getFloat(property.name, 0.0f)
+        Long::class -> getLong(property.name, 0L)
+        String::class -> getString(property.name, "")
+        else -> throw java.lang.UnsupportedOperationException()
+    } as T
+}
+
+operator fun <T> SharedPreferences.setValue(thisRef: Any, property: KProperty<*>, value: T) {
+    edit {
+        when (value) {
+            is Boolean -> putBoolean(property.name, value)
+            is Int -> putInt(property.name, value)
+            is Float -> putFloat(property.name, value)
+            is Long -> putLong(property.name, value)
+            is String -> putString(property.name, value)
+            else -> throw UnsupportedOperationException()
+        }
+
+    }
+}
